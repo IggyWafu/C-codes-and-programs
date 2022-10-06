04cae20c0ce7c6b5e0443c9662c1aedc76f2ee7c
IggyWafu <114693605+IggyWafu@users.noreply.github.com>
2022-10-06 12:08:27 +0300

    Add files via upload

---
+++ iggy.while.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main(){
+int i=1;
+while(i<=24){
+cout<<i<<" ";
+++i;
+}
+
+    return 0;
+}
---
+++ sum of even numbers iggy.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+      // Write C++ code here
+    int i,sum=0;
+    for(i=0;i<=50;i+=10)
+  sum=sum+i;{
+    std::cout << "sum="<<sum;
+}
+    return 0;
+}
